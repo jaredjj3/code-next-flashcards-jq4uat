a0888d0a1011608cfe0a5949d1e04ce4c1ce9be7
jaredjj3 <jared@jaredjohnson.dev>
2020-11-02 16:41:28 -0500

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Card } from './Card';
+import { Decks } from './Decks';
+import { Progress } from './Progress';
 
 export const App = () => {
   return (
     <div className="container">
       <h1>Flashcards</h1>
+      <Decks />
+      <hr />
+      <Progress />
+      <Card />
     </div>
   );
 };
---
+++ src/Decks.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Decks = () => {
+  return <div>Decks</div>;
+};
