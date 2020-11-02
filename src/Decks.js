ebeec407728860ab4fe76ae27f3240b7daa57882
jaredjj3 <jared@jaredjohnson.dev>
2020-11-02 17:14:19 -0500

    2 files modified

--- src/Decks.js
+++ src/Decks.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React from "react";
+import * as DeckAPI from "./data/DeckAPI";
+
+const DECKS = DeckAPI.getAllDecks();
 
 export const Decks = () => {
-  return <div>Decks</div>;
+  return (
+    <ul className="nav nav-pills">
+      {DECKS.map(deck => (
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            {deck.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 };
